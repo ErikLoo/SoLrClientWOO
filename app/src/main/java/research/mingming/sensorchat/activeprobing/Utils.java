e53a07fb0ace069cee260d1e0d41d237641fd325
Mingming Fan <mfan@cs.toronto.edu>
2018-05-27 18:17:08 -0400

    SoQrClient

---
+++ app/src/main/java/research/mingming/sensorchat/activeprobing/Utils.java
@@ -0,0 +1,8 @@
+package research.mingming.sensorchat.activeprobing;
+
+public final class Utils {
+	public static int probing_duration = 4000; // milliseconds
+	public static int repeat_period = 6000; // milliseconds  time interval between two adjacent probings
+
+	public static String targetObject = "1"; // 1:target object that beeps
+}
